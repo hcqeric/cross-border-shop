f6a0e2432cd6bb45b052c2f66e7ab0e5b70f1f3e
huangcq <huangcq@ibumobile.com>
2019-11-20 19:50:18 +0800

    提交公共头部代码

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="cbs-header">
         <div class="home-icon">
-
+            <img src="../assets/home@2x.png" alt="">
         </div>
         <div class="right-action">
-
+            <div>
+                <img src="../assets/cart@2x.png" alt="">
+            </div>
+            <div>
+                <img src="../assets/more@2x.png" alt="">
+            </div>
         </div>
     </div>
 </template>
@@ -16,5 +21,9 @@
 </script>
 
 <style scoped lang="less">
-
+.cbs-header{
+    width: 100vw;
+    height: 45px;
+    background: #000000;
+}
 </style>
